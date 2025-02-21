56669736bb0698752329f8f4fb2de3a28644ca90
Lucero Valdez <lvaldezd.isc91@gmail.com>
2025-02-20 23:03:44 -0600

    LVD
    add changes test

--- PythonRefresher/Variables/Variables.py
+++ PythonRefresher/Variables/Variables.py
@@ -2,21 +2,13 @@
 Variables
 """
 
-first_name = "Eric"
+first_name = "Lucero"
 print(first_name)
-first_name = "Melissa"
+first_name = "Juan"
 print(first_name)
 
+def get_full_name(first_name, last_name):
+    full_name = first_name.title() + " " + last_name.title()
+    return full_name
 
-
-
-
-
-
-
-
-
-
-
-
-
+print(get_full_name("john", "doe"))
